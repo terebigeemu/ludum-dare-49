243483174b1fde1ace38bc9ff02b7aced1b3823d
aidswidjaja <36395320+aidswidjaja@users.noreply.github.com>
2021-10-04 08:38:34 +1100

    More Level 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,14 +38,12 @@ public class Player : MonoBehaviour
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jump_height), ForceMode2D.Impulse);
             first_jump_done = true;
-            print("first jump");
 
         }
         else if ((Input.GetKeyDown("w") || Input.GetKeyDown("up")) && first_jump_done & !second_jump_done)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jump_height), ForceMode2D.Impulse);
             second_jump_done = true;
-            print("second jump");
         }
 
         if ((Input.GetKeyDown("a")) || Input.GetKeyDown("left"))
